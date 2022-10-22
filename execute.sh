3d169dc2a2e2274726871f4179a6e7cc40cddd58
jonas <>
2022-10-22 18:38:24 +0200

    fixed path:

--- execute.sh
+++ execute.sh
@@ -1,1 +1,1 @@
-python main.py --dataset="h36m" --path_to_data=./h36m_data --method_name="dummy_classifier" --use_cross_validation
+python main.py --dataset="h36m" --path_to_data=. --method_name="dummy_classifier" --use_cross_validation
