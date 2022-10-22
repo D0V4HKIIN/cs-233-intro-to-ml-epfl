a724ea3075e092e7574374a368c9af2976d177b1
jonas <>
2022-10-22 18:26:42 +0200

    simple execute script

--- execute.sh
+++ execute.sh
@@ -1,2 +1,1 @@
-python main.py --dataset="h36m" --path_to_data=<where you placed the
-data folder> --method_name="dummy_classifier" --use_cross_validation
+python main.py --dataset="h36m" --path_to_data=./h36m_data --method_name="dummy_classifier" --use_cross_validation
