37997fbe9fe0991e8285aa2e397dd02f8b0a2af8
jonas <>
2022-11-10 16:21:52 +0100

    cross validation on ridge and logistic

--- execute.sh
+++ execute.sh
@@ -1,1 +1,4 @@
-python main.py --dataset="h36m" --path_to_data=. --method_name="logistic_regression" --ridge_regression_lmda=0.0 --lr 0.000001 #--max_iters 10000 # --use_cross_validation
+echo "----------  ridge ----------"
+python3 main.py --dataset="h36m" --path_to_data=. --method_name="ridge_regression" --use_cross_validation
+echo "---------- logistic --------"
+python3 main.py --dataset="h36m" --path_to_data=. --method_name="logistic_regression" --use_cross_validation
